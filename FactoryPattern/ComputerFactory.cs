53f9fd3ce871b953b871dc78afd1113d584fc530
dinowilliam <dinowilliam@gmail.com>
2017-02-17 01:07:08 -0200

    Include Factory Pattern

---
+++ FactoryPattern/ComputerFactory.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryPattern
+{
+    public class ComputerFactory{
+       
+            public ComputerProduct GetComputer(int type) {
+
+                switch (type) {
+                    case 1:
+                        return new IBMComputerProduct();
+                    case 2:
+                        return new COMPAQComputerProduct();
+                    default:
+                        throw new NotSupportedException();
+                }
+            
+            }
+    }
+}
